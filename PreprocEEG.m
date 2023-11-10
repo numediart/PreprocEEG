0bc65784e65dc5c7a07950dbf730991acc437b93
LucaLaFisca <lucalafisca@gmail.com>
2023-11-10 14:21:01 +0100

    Update License

--- PreprocEEG.m
+++ PreprocEEG.m
@@ -1,4 +1,24 @@
 function [multi_timelock] = PreprocEEG(config_file)
+%{
+Created by Luca La Fisca
+ISIA Lab, Faculty of Engineering University of Mons, Mons (Belgium)
+luca.lafisca@umons.ac.be
+Source: La Fisca et al, "Explainable AI for EEG Biomarkers Identification in Obstructive Sleep Apnea Severity Scoring Task", NER 2023.
+Copyright (C) 2023 - UMons
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+This library cannot be used for commercial use without the agreement of the
+author (Luca La Fisca).
+%}
 
 %read config file
 fname = config_file;
